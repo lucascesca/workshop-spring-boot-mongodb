7919812ff92649c157d6b7cf1aea5bcae81879eb
Lucas Cesca <lucascesca14@gmail.com>
2024-01-28 20:57:43 -0300

    User Entity

---
+++ src/main/java/com/lucascesca/workshopmongodb/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.lucascesca.workshopmongodb.resources;
+
+import com.lucascesca.workshopmongodb.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@email.com");
+        User alex = new User("2", "Alex Green", "alex@email.com");
+        List<User> list = new ArrayList<>(Arrays.asList(maria, alex));
+
+        return ResponseEntity.ok().body(list);
+    }
+}
