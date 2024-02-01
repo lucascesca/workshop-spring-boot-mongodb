c984acdbeda4a86fa0e864cb88ed76bc406aca8e
Lucas Cesca <lucascesca14@gmail.com>
2024-01-31 23:35:18 -0300

    Implementing findById

--- src/main/java/com/lucascesca/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/lucascesca/workshopmongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.lucascesca.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,10 @@ public class UserResource {
         List<UserDTO> dtoList = users.stream().map(x -> new UserDTO(x)).toList();
 
         return ResponseEntity.ok().body(dtoList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        return ResponseEntity.ok().body(new UserDTO(service.findById(id)));
     }
 }
