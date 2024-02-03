ca3832627c490d21fb21e9f033b94c1ee3d5287e
Lucas Cesca <lucascesca14@gmail.com>
2024-02-02 21:14:10 -0300

    Post entity

---
+++ src/main/java/com/lucascesca/workshopmongodb/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.lucascesca.workshopmongodb.repositories;
+
+import com.lucascesca.workshopmongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
