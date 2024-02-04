e8ce762a40a223ee7930875fb3637c9c789dae0e
Lucas Cesca <lucascesca14@gmail.com>
2024-02-04 12:02:33 -0300

    Search with @Query

--- src/main/java/com/lucascesca/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/lucascesca/workshopmongodb/repositories/PostRepository.java
@@ -2,12 +2,17 @@ package com.lucascesca.workshopmongodb.repositories;
 
 import com.lucascesca.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     //Query method
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
